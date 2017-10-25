82ee95e7f792763227f06007d0fc9731a202ef83
IvanaSkoric <i.skoric09@gmail.com>
2017-10-25 14:32:45 +0200

    uradjen peti

--- 4.c
+++ 4.c
@@ -1,6 +1,4 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<assert.h>
+
  
 /* Link list node */
 Nodestruct node
---
+++ day2/5.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+
+
+struct Node 
+{
+	int data;
+	struct Node *prev;
+	struct Node *next;
+	}Node;
+	
+	
+void push (struct Node** head, int value)
+{
+	struct Node* new_node=(struct Node *)malloc(sizeof(struct Node));
+	
+	
+	new_node->data=value; //insert data 
+	
+	new_node->next=(*head); //next of new node is head
+	new_node->prev=NULL;	//prev of new node is null
+	
+	if((*head) != NULL)		//prev of head node is the new node
+		(*head)->prev=new_node;
+		
+	 (*head)=new_node; //head is now the new_node
+	 
+	 }
+	 
+void printList (struct Node *node)
+{
+
+	struct Node *fast=node;
+	struct Node *slow=node;
+	struct Node *lijevo;
+	if (node != NULL)
+		{
+			while (fast != NULL && fast->next !=NULL)
+				{
+					fast=fast->next->next;
+					slow=slow->next;
+					
+					} printf("Niz %d", slow->data);
+					lijevo=slow;
+					while (lijevo->next != NULL && slow->prev != NULL)
+					{
+						lijevo=lijevo->next;
+						slow=slow->prev;
+						printf("%d", slow->data);
+						printf ("%d", lijevo->data);
+						
+						}
+
+			}	
+	
+}
+
+	
+
+
+int main()
+{
+	struct Node *node=NULL;
+	int a, i;
+	for (i=0; i<9; i++)
+	{
+		printf("Enter the nubmer:");
+		scanf("%d", &a);
+		push(&node, a);
+		
+		}
+	
+	
+	printList(node);
+
+	return 0;
+}
